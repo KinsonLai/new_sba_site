4c1e29e806cbd4af1dab42cc32327c0b7dacf7cd
KinsonLai <KinsonLai232@gmail.com>
2023-12-01 17:00:48 +0800

    Add functions

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    comment = db.Column(db.Text)
+    rating = db.Column(db.Float, nullable=False, default=0.0)
+    num_ratings = db.Column(db.Integer, nullable=False, default=0)
     image_path = db.Column(db.String(255), nullable=True)
     description = db.Column(db.Text)
     latest_product = db.Column(db.Boolean, default=False)
     popular_product = db.Column(db.Boolean, default=False)
+    comments = db.relationship('Comment', backref='product', lazy=True)
 
 
 class User(db.Model):
@@ -30,6 +31,19 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+
+class CommentForm(FlaskForm):
+    comment = StringField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Add Comment')
 
 
 # Registration form
@@ -65,18 +79,47 @@ def home():
     popular_products = Product.query.filter_by(popular_product=True).all()
     return render_template('home.html', latest_products=latest_products, popular_products=popular_products)
 
-@app.route('/product/<int:product_id>', methods=['GET'])
+@app.route('/product/<int:product_id>', methods=['GET', 'POST'])
 def view_product(product_id):
     product = Product.query.get_or_404(product_id)
-    return render_template('product.html', product=product)
+    form = CommentForm()
+    rating_form = RatingForm()
+
+    if form.validate_on_submit():
+        rating = int(request.form['rating'])
+        comment = Comment(username=session['username'], product_id=product.id, comment=form.comment.data, rating=rating)
+
+        # Update the product's rating and number of ratings
+        update_stmt = update(Product).where(Product.id == product.id).values(
+            rating=((Product.rating * Product.num_ratings) + rating) / (Product.num_ratings + 1),
+            num_ratings=Product.num_ratings + 1
+        )
+        db.session.execute(update_stmt)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Comment and rating added successfully!', 'success')
+        return redirect(url_for('view_product', product_id=product.id))
+
+    if rating_form.validate_on_submit():
+        rating = int(request.form['rating'])
+
+        # Update the product's rating and number of ratings
+        update_stmt = update(Product).where(Product.id == product.id).values(
+            rating=((Product.rating * Product.num_ratings) + rating) / (Product.num_ratings + 1),
+            num_ratings=Product.num_ratings + 1
+        )
+        db.session.execute(update_stmt)
+        db.session.commit()
+        flash('Rating submitted successfully!', 'success')
+        return redirect(url_for('view_product', product_id=product.id))
+
+    return render_template('product.html', product=product, form=form, rating_form=rating_form)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
         name = request.form['name']
         price = float(request.form['price'])
-        rating = float(request.form['rating'])
-        comment = request.form['comment']
         description = request.form['description']
         latest_product = 'latest_product' in request.form
         popular_product = 'popular_product' in request.form
@@ -90,7 +133,7 @@ def admin():
         else:
             image_path = None
 
-        product = Product(name=name, price=price, rating=rating, comment=comment,
+        product = Product(name=name, price=price,
                           image_path=image_path, description=description,
                           latest_product=latest_product, popular_product=popular_product)
         db.session.add(product)
@@ -174,7 +217,6 @@ def register():
 
     return render_template('register.html', form=form)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -193,6 +235,14 @@ def login():
         return redirect('/')
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    # Clear the session data
+    session.clear()
+    # Redirect to the home page or any other page after logout
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     db.create_all()
